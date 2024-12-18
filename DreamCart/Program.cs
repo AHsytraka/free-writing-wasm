36dcfa1841a7ede39647140dba8035dd5d315b6a
AHsytraka <ahsytraka@gmail.com>
2024-12-18 11:08:54 +0300

    changed project to pwa

--- DreamCart/Program.cs
+++ DreamCart/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using free_writing_wasm;
+using DreamCart;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
